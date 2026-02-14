b5ef1084d4333a73af0bed684014eef40c4b816c
IDGS-805-23002384 <mayherar7478@gmail.com>
2026-02-13 21:13:03 -0600

    Configuración 13/02/26

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import create_engine
+
+class Config(object):
+    SECRET_KEY = "ClaveSecreta"
+    SESSION_COOKIE_SECURE=False
+    
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:2390L@127.0.0.1/bdidgs805'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
