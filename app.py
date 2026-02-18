38d05444c0873263ef97e2202094e9d616d21502
IDGS-805-23002384 <mayherar7478@gmail.com>
2026-02-18 00:01:00 -0600

    Insertar 17/feb/26

--- app.py
+++ app.py
@@ -1,19 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+from config import DevelopmentConfig
+
+# from flask_migrate import Migrate
+import forms
+
+from models import db, Alumnos
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+db.init_app(app)
+csrf = CSRFProtect()
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    create_form = forms.UserForm(request.form)
+    alumno = Alumnos.query.all()
+    return render_template("index.html", form=create_form, alumno=alumno)
+
 
 @app.errorhandler(404)
 def page_not_fount(e):
-    return render_template ("404.html"), 404
+    return render_template("404.html"), 404
 
-@app.route("/Alumnos")
+
+@app.route("/Alumnos", methods=["GET", "POST"])
 def alumnos():
-    return render_template("Alumnos.html")
+    create_form = forms.UserForm(request.form)
+    if request.method == "POST":
+        alum = Alumnos(
+            nombre=create_form.nombre.data,
+            apaterno=create_form.apaterno.data,
+            email=create_form.email.data,
+        )
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("Alumnos.html", form=create_form)
 
-if __name__ == '__main__':
-	app.run(debug=True)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+app.run()
